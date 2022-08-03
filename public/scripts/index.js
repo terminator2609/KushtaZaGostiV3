6c8abcc5022fcd77dcf7a8b82320f55b4b90504a
Radostin Dimitrov <radostin1234567891122@abv.bg>
2022-08-03 22:27:25 +0300

    create ,,add object'' page

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -6,10 +6,26 @@ let navMenuIcon = document.getElementsByClassName("nav-menu-icon")[0]
 let navMenu = document.getElementsByClassName("nav-menu-phone")[0]
 let questionButton = document.getElementsByClassName("fa-circle-question")
 let definitionDiv = document.getElementsByClassName("definition")
+let headerArray = ["НЕКА ПРИКЛЮЧЕНИЕТО ЗАПОЧНЕ", "ВАШЕТО УНИКАЛНО РЕВЮ", "ДА ПОРАБОТИМ И ВЪРХУ МЕДИЯТА", "ВАШАТА ЦЕНА", "СОЦИАЛНИ МРЕЖИ", "НАЙ-ИНТЕРЕСНАТА ЧАСТ", ""]
+let nextButtons = document.getElementsByClassName("next-step")
+let addObjectSections = document.getElementsByClassName("section-add")
+let addObjectTitle = document.getElementById("addObject-title")
+let reviewTextArea = document.getElementById("review")
+let cataloguesSelectPhoto = document.getElementsByClassName("cataloguesSelectPhoto")
+let allSelectCommand = document.getElementById("selectAllCatalogues")
+let removeAllSelectionCommand = document.getElementById("removeAllSelection")
+let currentMonth = document.getElementById("current-month")
+let currentNumberMonth = document.getElementById("current-number-month")
+let currentDate = document.getElementById("current-date")
+let dateNumber = document.getElementsByClassName("date-number")
 
 
 for (let i = 0; i < definitionDiv.length; i++) {
     definitionDiv[i].style.display = "none"
+}
+
+for (let i = 1; i < addObjectSections.length; i++) {
+    addObjectSections[i].style.display = "none"
 }
 
 navMenu.style.display = "none"
@@ -162,15 +178,284 @@ for (let i = 0; i < questionButton.length; i++) {
     questionButton[i].addEventListener("click", (e) => {
         e.preventDefault()
 
-        console.log("clicked")
 
-        if(definitionDiv[i].style.display === "none") {
+        if (definitionDiv[i].style.display === "none") {
             definitionDiv[i].style.display = "flex"
         } else {
             definitionDiv[i].style.display = "none"
         }
     })
 }
+
+for (let i = 0; i < nextButtons.length; i++) {
+
+
+    nextButtons[i].addEventListener("click", (e) => {
+        e.preventDefault()
+
+        addObjectTitle.textContent = headerArray[i + 1]
+        addObjectSections[i].style.display = "none"
+        addObjectSections[i + 1].style.display = "flex"
+
+        if (document.getElementsByClassName("catalogues-select-villa")[0].style.display === "flex") {
+            document.getElementsByTagName("main")[0].style.marginTop = "0%"
+        }
+
+    })
+
+
+
+
+}
+
+reviewTextArea.addEventListener("click", (e) => {
+    e.preventDefault()
+
+    reviewTextArea.cols = 40
+    reviewTextArea.rows = 5
+})
+
+for (let i = 0; i < cataloguesSelectPhoto.length; i++) {
+
+    allSelectCommand.addEventListener("click", (e) => {
+        e.preventDefault()
+
+        cataloguesSelectPhoto[i].className = "cataloguesSelectPhoto selectedCatalogues"
+    })
+
+    removeAllSelectionCommand.addEventListener("click", (e) => {
+        e.preventDefault()
+
+        cataloguesSelectPhoto[i].className = "cataloguesSelectPhoto"
+    })
+
+    cataloguesSelectPhoto[i].addEventListener("click", (e) => {
+        e.preventDefault()
+
+        if (cataloguesSelectPhoto[i].className === "cataloguesSelectPhoto selectedCatalogues") {
+            cataloguesSelectPhoto[i].className = "cataloguesSelectPhoto"
+        } else {
+            cataloguesSelectPhoto[i].className = "cataloguesSelectPhoto selectedCatalogues"
+        }
+    })
+
+
+}
+
+calender()
+
+function calender() {
+
+    let month = ["Януари", "Февруари", "Март", "Април", "Май", "Юни", "Юли", "Август", "Септември", "Октомври", "Ноември", "Декември"]
+
+    let dateObject = new Date()
+
+    let currentMonthContent = dateObject.getMonth()
+    let currentDateContent = dateObject.getDate()
+    let currentFullYearContent = dateObject.getFullYear()
+
+    currentMonth.textContent = month[dateObject.getMonth()]
+    currentNumberMonth.textContent = `00${dateObject.getMonth() + 1}`
+    currentDate.textContent = `0${dateObject.getDate()}.0${dateObject.getMonth() + 1}.${dateObject.getFullYear()}`
+
+    for (let i = 0; i < dateNumber.length; i++) {
+
+        if (i === 0) {
+            dateObject.setDate(1)
+        }
+
+        if (dateObject.getDay() > 1 || dateObject.getDay() === 0 && i === 0) {
+
+            if (dateObject.getDay() === 0) {
+                // dateObject.setMonth(dateObject.getMonth() - 1)
+
+                if (dateObject.getMonth() - 1 % 2 === 0 || dateObject.getMonth() - 1 === 7 || dateObject.getMonth() - 1 === -1) {
+                    for (let b = 0; b < 6; b++) {
+                        dateNumber[i].textContent = 26 + b
+                        i++
+                    }
+                } else if (dateObject.getMonth() - 1 === 1) {
+
+                    if (dateObject.getFullYear() % 4 === 0) {
+                        for (let b = 0; b < 6; b++) {
+                            dateNumber[i].textContent = 24 + b
+                            i++
+                        }
+                    } else {
+                        for (let b = 0; b < 6; b++) {
+                            dateNumber[i].textContent = 23 + b
+                            i++
+                        }
+                    }
+
+                } else {
+                    for (let b = 0; b < 6; b++) {
+                        dateNumber[i].textContent = 25 + b
+                        i++
+                    }
+                }
+            } else {
+
+                if (dateObject.getMonth() - 1 % 2 === 0 || dateObject.getMonth() - 1 === 7 || dateObject.getMonth() - 1 === -1) {
+
+                    let latestDay = 31 - dateObject.getDay()
+
+                    for (let b = 1; b < dateObject.getDay(); b++) {
+                        dateNumber[i].textContent = latestDay + b
+                        i++
+                    }
+                } else if (dateObject.getMonth() - 1 === 1) {
+
+                    if (dateObject.getFullYear() % 4 === 0) {
+
+                        let latestDay = 29 - dateObject.getDay()
+
+                        for (let b = 1; b < dateObject.getDay(); b++) {
+                            dateNumber[i].textContent = latestDay + b
+                            i++
+                        }
+                    } else {
+
+                        let latestDay = 28 - dateObject.getDay()
+
+                        for (let b = 1; b < dateObject.getDay(); b++) {
+                            dateNumber[i].textContent = latestDay + b
+                            i++
+                        }
+                    }
+
+                } else {
+
+                    let latestDay = 30 - dateObject.getDay()
+
+                    for (let b = 1; b < dateObject.getDay(); b++) {
+                        dateNumber[i].textContent = latestDay + b
+                        i++
+                    }
+                }
+            }
+
+        } else {
+
+            if (dateObject.getMonth() % 2 === 0 || dateObject.getMonth() === 7 || dateObject.getMonth() === 11) {
+                for (let b = 1; b <= 31; b++) {
+
+                    if (b < 10) {
+                        dateNumber[i].textContent = `0${b}`
+                    } else {
+                        dateNumber[i].textContent = b
+                    }
+
+                    i++
+                }
+
+                if (i >= 31 && i <= 41) {
+
+                    let firstNumbersNewMonth = 42 - i
+
+                    for (let b = 1; b <= firstNumbersNewMonth; b++) {
+
+                        if (b < 10) {
+                            dateNumber[i].textContent = `0${b}`
+                        } else {
+                            dateNumber[i].textContent = b
+                        }
+
+                        i++
+                    }
+                }
+
+            } else if (dateObject.getMonth() - 1 === 1) {
+
+                if (dateObject.getFullYear() % 4 === 0) {
+                    for (let b = 1; b <= 29; b++) {
+
+                        if (b < 10) {
+                            dateNumber[i].textContent = `0${b}`
+                        } else {
+                            dateNumber[i].textContent = b
+                        }
+
+                        i++
+                    }
+
+                    if (i >= 29 && i <= 41) {
+                        for (let b = 1; b <= 42 - i; b++) {
+
+                            if (b < 10) {
+                                dateNumber[i].textContent = `0${b}`
+                            } else {
+                                dateNumber[i].textContent = b
+                            }
+
+                            i++
+                        }
+                    }
+
+                } else {
+                    for (let b = 1; b <= 28; b++) {
+
+                        if (b < 10) {
+                            dateNumber[i].textContent = `0${b}`
+                        } else {
+                            dateNumber[i].textContent = b
+                        }
+
+                        i++
+                    }
+
+                    if (i >= 28 && i <= 41) {
+                        for (let b = 1; b <= 42 - i; b++) {
+
+                            if (b < 10) {
+                                dateNumber[i].textContent = `0${b}`
+                            } else {
+                                dateNumber[i].textContent = b
+                            }
+
+                            i++
+                        }
+                    }
+                }
+
+            } else {
+                for (let b = 1; b <= 30; b++) {
+
+                    if (b < 10) {
+                        dateNumber[i].textContent = `0${b}`
+                    } else {
+                        dateNumber[i].textContent = b
+                    }
+
+                    i++
+                }
+
+                if (i >= 30 && i <= 41) {
+                    for (let b = 1; b <= 42 - i; b++) {
+
+                        if (b < 10) {
+                            dateNumber[i].textContent = `0${b}`
+                        } else {
+                            dateNumber[i].textContent = b
+                        }
+
+                        i++
+                    }
+                }
+            }
+
+        }
+
+
+    }
+
+    
+
+}
+
+
+
+
 
 
 
